e0c24c3c63cde895715cc210226dad2b8a3217d8
kyle <kshepherd@bulogics.com>
2014-05-31 14:32:48 -0400

    changes dontfall.ino to the mx2125 sample code

--- src/dontfall/dontfall.ino
+++ src/dontfall/dontfall.ino
@@ -1,50 +1,63 @@
 /*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13. 
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead(). 
- 
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +5V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
- 
- * Note: because most Arduinos have a built-in LED attached 
- to pin 13 on the board, the LED is optional.
- 
- 
- Created by David Cuartielles
- modified 30 Aug 2011
- By Tom Igoe
- 
- This example code is in the public domain.
- 
- http://arduino.cc/en/Tutorial/AnalogInput
- 
+   Memsic2125
+   
+   Read the Memsic 2125 two-axis accelerometer.  Converts the
+   pulses output by the 2125 into milli-g's (1/1000 of earth's
+   gravity) and prints them over the serial connection to the
+   computer.
+   
+   The circuit:
+	* X output of accelerometer to digital pin 2
+	* Y output of accelerometer to digital pin 3
+	* +V of accelerometer to +5V
+	* GND of accelerometer to ground
+  
+   http://www.arduino.cc/en/Tutorial/Memsic2125
+   
+   created 6 Nov 2008
+   by David A. Mellis
+   modified 30 Aug 2011
+   by Tom Igoe
+   
+   This example code is in the public domain.
+
  */
 
-int sensorPin = A2;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+// these constants won't change:
+const int xPin = 2;		// X output of the accelerometer
+const int yPin = 3;		// Y output of the accelerometer
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);  
+  // initialize serial communications:
+  Serial.begin(9600);
+  // initialize the pins connected to the accelerometer
+  // as inputs:
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
 }
 
 void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);    
-  // turn the ledPin on
-  digitalWrite(ledPin, HIGH);  
-  // stop the program for <sensorValue> milliseconds:
-  delay(sensorValue);          
-  // turn the ledPin off:        
-  digitalWrite(ledPin, LOW);   
-  // stop the program for for <sensorValue> milliseconds:
-  delay(sensorValue);                  
+  // variables to read the pulse widths:
+  int pulseX, pulseY;
+  // variables to contain the resulting accelerations
+  int accelerationX, accelerationY;
+  
+  // read pulse from x- and y-axes:
+  pulseX = pulseIn(xPin,HIGH);  
+  pulseY = pulseIn(yPin,HIGH);
+  
+  // convert the pulse width into acceleration
+  // accelerationX and accelerationY are in milli-g's: 
+  // earth's gravity is 1000 milli-g's, or 1g.
+  accelerationX = ((pulseX / 10) - 500) * 8;
+  accelerationY = ((pulseY / 10) - 500) * 8;
+
+  // print the acceleration
+  Serial.print(accelerationX);
+  // print a tab character:
+  Serial.print("\t");
+  Serial.print(accelerationY);
+  Serial.println();
+
+  delay(100);
 }
